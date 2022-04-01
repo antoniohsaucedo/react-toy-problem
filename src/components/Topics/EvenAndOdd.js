1df60d5f99faafc36728a8b0d1bd5443a51255be
antoniohsaucedo <antoniohsaucedo.com>
2022-03-31 23:41:56 -0600

    first commit

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+export default class EvenAndOdd extends Component {
+    render() {
+      return (
+
+<div className="puzzleBox evenAndOddPB">
+ <h4> Evens and Odds </h4>
+      <input className="inputLine"> </input>
+       <button className="confirmationButton"> </button>
+      <span className="resultsBox"> </span>
+      </div>
+      )
+    }
+  }
